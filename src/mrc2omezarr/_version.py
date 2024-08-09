06a49d45f0a46ac52f3451228c7d5030690e6062
uermel <uermel@czii.org>
2024-08-09 12:05:28 -0700

    fix version location

--- src/mrc2omezarr/_version.py
+++ src/mrc2omezarr/_version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.2"
-__version_tuple__ = version_tuple = (0, 2)
+__version__ = version = "0.3.dev4+gd6164cf.d20240809"
+__version_tuple__ = version_tuple = (0, 3, "dev4", "gd6164cf.d20240809")
